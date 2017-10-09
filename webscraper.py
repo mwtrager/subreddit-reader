3ad080a9815043a7859b8b4c39301c1ed09041be
= <mtrager20@gmail.com>
2017-10-08 23:01:56 -0400

    explicit naming

--- webscraper.py
+++ webscraper.py
@@ -13,13 +13,13 @@ html = r.text
 
 soup = make_me_soup(html, 'html.parser')
 
-# when there is only one link on the page, it means we were detected as a bot
+# when there is only one <a> on the page, it means we were detected as a bot
 def we_are_bot():
-    links = []
-    for link in soup.find_all('a'):
-        links.append(link)
+    a_tags = []
+    for tag in soup.find_all('a'):
+        a_tags.append(tag)
 
-    return len(links) == 1
+    return len(a_tags) == 1
 
 if we_are_bot():
     print('detected as bot :(')
@@ -32,6 +32,7 @@ else:
     divs = divs[1:]
 
     # build word lists from comments
+    # TODO need nltk here
     comments_wordlists = []
     for div in divs:
         child = div.find('p')
