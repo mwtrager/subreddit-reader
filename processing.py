9976594cf087a84aee4b8540f54e70bc4f83c696
Matthew trager <mtrager-home@Matthews-MacBook-Air.local>
2017-10-13 08:07:19 -0400

    re-organized code and named vars and funcs better

--- processing.py
+++ processing.py
@@ -3,24 +3,25 @@ import nltk
 
 def tokenize(entries):
     # TODO error check
-    # how to handle links that people put in?
     # BUG I'm getting garbage data from links and shit
+    # NOTE This returns a list of comments, so using len(tokens) returns op + # of comments
     return [nltk.word_tokenize(entry) for entry in entries]
 
-# TODO rename and fix
-# BUG using isalpha ignores some data like if someone typed '9gag' maybe
-# returns unique words used in the whole reddit post
-def get_vocab(entries):
-    # put all words into same list to make this easier
-    entry_words = []
+def get_words(entries):
+    # TODO change variable name? tokens is a little ambiguous...
+    # BUG using isalpha ignores some data like if someone typed '9gag' maybe
     all_words = []
-    # so for each entry, append words to entry_words
+    entry_words = []
     for entry in entries:
         entry_words.append([word.lower() for word in entry if word.isalpha()])
-        [all_words.append(word) for entry in entry_words for word in entry] # list comprehension. is this coshure?
-        # TODO need a function to return all_words so that I can easily calculate lexical diversity
-        vocab = sorted(set(all_words))
-    return vocab
+        [all_words.append(word) for entry in entry_words for word in entry] # list comprehension. is this koshur?
+    return all_words
+
+def get_vocab(words):
+    return set(words)
+
+def lexical_diversity(words, vocab):
+    return len(vocab)/len(words)
 
 # def stress(pron):
 # ...     return [char for phone in pron for char in phone if char.isdigit()]
