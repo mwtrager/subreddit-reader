8d5f6b2d4034657237fe936330e87077c15ee8f1
mwtrager <mtrager20@gmail.com>
2017-10-28 13:21:34 -0400

    test joke reading in main

--- main.py
+++ main.py
@@ -24,19 +24,28 @@ def test_unusual_words(urls):
 
     return 1
 
+# TODO change the name ya dingus
+def test_stuff(urls):
+    # i want to get all the unusal words from each url in urls
+    for url in urls:
+        print('trying to get ' + url)
+        soup = human_soup(url)
+        print('----')
+        print('')
+        title = get_post_titles(soup)[0]
+        divs = get_divs(soup)
+        text = get_raw(divs)
+        print(title)
+        print('')
+        print(text[0])
+    return 1
+
+
 # -- START -- #
-print('get all unusual words from all the posts on a subreddits frontpage')
+print('get some jokes atm')
 base_url = 'https://www.reddit.com/r/'
 subreddit = input('enter a subreddit: ') # TODO error checking needs to happen here
 sub_soup = human_soup(base_url + subreddit)
-titles = get_post_titles(sub_soup)
-
-# TODO use these titles to give an overview of posts that will be ripped
-    # and at the head of each loop so you give a good title
-print('\n')
-print('Posts to be ripped from', subreddit, '\n')
-[print(title) for title in titles]
-
-# post_divs = get_post_divs(sub_soup)
-# post_links = get_post_links(post_divs)
-# test_unusual_words(post_links)
+post_divs = get_post_divs(sub_soup)
+post_links = get_post_links(post_divs)
+test_stuff(post_links)
